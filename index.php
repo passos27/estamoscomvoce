8850267f01d90871a08ad833aaaeae56f5a96248
Francisco Passos <passos.27@hotmail.com>
2018-11-25 19:46:43 -0800

    media querie

--- index.php
+++ index.php
@@ -1,6 +1,8 @@
 		<!DOCTYPE html>
 		<html>
 		<head>
+			<meta charset="UTF-8">
+			<meta name="viewport" content="width=device-width, initial-scale=1">
 			<title><?php bloginfo('name')?></title>
 			<link rel="stylesheet" type="text/css" href="<?php bloginfo('template_directory')?>/style.css">
 				<link rel="stylesheet" type="text/css" href="<?php bloginfo('template_directory')?>/css/materialize.min.css">
